a1f0eca6896df78e05ce50232dd4db6be1b7868d
Isabel Costa <isabelcmdcosta@gmail.com>
2017-06-23 22:51:30 +0100

    create and new action created for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+  public
   # Show all users
   def index
     @users = User.all()
@@ -12,5 +13,34 @@ class UsersController < ApplicationController
 
   # Create user after filling form
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path()
+    else
+      render 'new'
+    end
   end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+    @user.save
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email)
+  end
+
 end
